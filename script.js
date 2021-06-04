ad71b6deef5db09eb64e57c4c731ea86d594adac
vlucats <vitorpk@gmail.com>
2021-06-04 15:48:38 -0300

    Finalizado!

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle= "black";
@@ -20,9 +25,38 @@ function criarSnake(){
     }
 }
 
+function drawFood(){
+    context.fillStyle="red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction !="up") direction = "down";
+
+}
+
 function iniciarJogo(){
+    let score=0;
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;    
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :´(');
+        }
+    }
+    
     criarBG();
     criarSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -30,9 +64,18 @@ function iniciarJogo(){
     if(direction == "right"){ snakeX +=box};
     if(direction == "left") { snakeX -=box};
     if(direction == "up")   { snakeY -=box};
-    if(direction == "down") { snakeY -=box};
+    if(direction == "down") { snakeY +=box};
 
-    snake.pop();
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x=Math.floor(Math.random() * 15 + 1) * box;
+        food.y=Math.floor(Math.random() * 15 + 1) * box;
+        score = score+1;
+        }
+
+ 
 
     let newHead ={
         x:snakeX,
